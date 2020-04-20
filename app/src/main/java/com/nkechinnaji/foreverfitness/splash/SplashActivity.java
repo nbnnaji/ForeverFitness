0a110d904e70919529bb51a2f33aca4f40e28921
nbnnaji <nbnnaji@yahoo.co.uk>
2020-04-19 21:45:04 -0500

    Reduced onboarding time

--- app/src/main/java/com/nkechinnaji/foreverfitness/splash/SplashActivity.java
+++ app/src/main/java/com/nkechinnaji/foreverfitness/splash/SplashActivity.java
@@ -11,7 +11,7 @@ import com.nkechinnaji.foreverfitness.onboarding.OnBoardingActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
-    private static int SPLASH_TIME_OUT = 10000;
+    private static int SPLASH_TIME_OUT = 4000;
 
 
     @Override
